f0f099fbe5bbb7e05fe27894266ad822f47154b9
Felix Vítek <felox8@gmail.com>
2023-05-06 12:23:52 +0200

    Add simple math input component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import App from '@/App'
 
+import '@/styles/index.css'
+import 'mathlive'
+
 createRoot(document.getElementById('app') as HTMLElement).render(
   <StrictMode>
     <App />
