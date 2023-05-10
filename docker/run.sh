bf8533b61e61a6d45d7b077b6d58293c94ffdb58
Felix Vítek <felox8@gmail.com>
2023-05-10 19:09:39 +0200

    Fix python & images in docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-php artisan migrate --force
-php artisan db:seed --force -q
+if [ ! -e './first_start' ]; then
+    sleep 5
+    touch ./first_start
+    php artisan migrate --force
+    php artisan db:seed --force
+fi
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
